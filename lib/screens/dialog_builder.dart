08a81cb49d5a9aba86f31ce2327375100a410f58
Han Park <hspark15429@gmail.com>
2023-07-18 23:46:53 +0900

    fixed willpopscope of timetable entry dialog

--- lib/screens/dialog_builder.dart
+++ lib/screens/dialog_builder.dart
@@ -12,63 +12,66 @@ Future<String?> timetableEntryDialogBuilder(
     builder: (BuildContext context) {
       String searchResult = currentLecture;
       return StatefulBuilder(builder: (context, setState) {
-        return Dialog.fullscreen(
-          child: Container(
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    IconButton(
-                        onPressed: () {
-                          Navigator.of(context).pop('');
-                        },
-                        icon: Icon(Icons.cancel)),
-                    Header('Search Lecture'),
-                    TextButton(
-                        onPressed: () {
-                          Navigator.of(context).pop('Save$searchResult');
-                        },
-                        child: Text('Save')),
-                    TextButton(
-                        onPressed: () {
-                          Navigator.of(context).pop('Delete');
-                        },
-                        child: Text('Delete')),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Paragraph('Find lecture by name or professor'),
-                  ],
-                ),
-                Row(
-                  children: [
-                    IconButton(
-                      onPressed: () async {
-                        searchResult = await showSearch(
-                          context: context,
-                          delegate:
-                              CustomSearchDelegate(cellText: currentCellNum),
-                        );
-                        setState(() {
-                          searchResult = searchResult;
-                        });
-                      },
-                      icon: const Icon(Icons.search),
-                    )
-                  ],
-                ),
-                SafeArea(
-                  child: Row(
+        return WillPopScope(
+          onWillPop: () async => false,
+          child: Dialog.fullscreen(
+            child: Container(
+              child: Column(
+                children: [
+                  Row(
                     children: [
-                      Flexible(
-                          child: Text(searchResult.isEmpty
-                              ? 'Your lecture: $currentLecture'
-                              : 'Your lecture: $searchResult')),
+                      IconButton(
+                          onPressed: () {
+                            Navigator.of(context).pop('');
+                          },
+                          icon: Icon(Icons.cancel)),
+                      Header('Search Lecture'),
+                      TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop('Save$searchResult');
+                          },
+                          child: Text('Save')),
+                      TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop('Delete');
+                          },
+                          child: Text('Delete')),
                     ],
                   ),
-                ),
-              ],
+                  Row(
+                    children: [
+                      Paragraph('Find lecture by name or professor'),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: () async {
+                          searchResult = await showSearch(
+                            context: context,
+                            delegate:
+                                CustomSearchDelegate(cellText: currentCellNum),
+                          );
+                          setState(() {
+                            searchResult = searchResult;
+                          });
+                        },
+                        icon: const Icon(Icons.search),
+                      )
+                    ],
+                  ),
+                  SafeArea(
+                    child: Row(
+                      children: [
+                        Flexible(
+                            child: Text(searchResult.isEmpty
+                                ? 'Your lecture: $currentLecture'
+                                : 'Your lecture: $searchResult')),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         );
