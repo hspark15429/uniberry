485ed6718359e5e6c219d2e40007c9f8bd392d2f
Han Park <hspark15429@gmail.com>
2023-08-06 18:00:45 +0900

    bug fix

--- lib/screens/dialog_builder.dart
+++ lib/screens/dialog_builder.dart
@@ -7,72 +7,75 @@ Future<String?> timetableEntryDialogBuilder(
   required String currentLecture,
   required String currentCellNum,
 }) async {
-  return showDialog<String>(
+  String newLecture = "";
+  return showModalBottomSheet<String>(
     context: context,
     builder: (BuildContext context) {
-      String searchResult = currentLecture; // currentLecture;
       return StatefulBuilder(builder: (context, setState) {
         return WillPopScope(
-          onWillPop: () async => true,
-          child: Dialog.fullscreen(
-            child: Container(
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      IconButton(
-                          onPressed: () {
-                            Navigator.of(context).pop('');
-                          },
-                          icon: Icon(Icons.cancel)),
-                      Header('講義検索'),
-                      TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop('Save$searchResult');
-                          },
-                          child: Text('保存')),
-                      TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop('Delete');
-                          },
-                          child: Text('削除')),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Paragraph('講義名または教授名で検索'),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      IconButton(
-                        onPressed: () async {
-                          searchResult = await showSearch(
-                            context: context,
-                            delegate: CustomSearchDelegate(
-                              cellText: currentCellNum,
-                            ),
-                          );
-                          setState(() {
-                            searchResult = searchResult;
-                          });
+          onWillPop: () async => false,
+          child: Container(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    IconButton(
+                        onPressed: () {
+                          Navigator.of(context).pop('');
                         },
-                        icon: const Icon(Icons.search),
-                      )
+                        icon: Icon(Icons.cancel)),
+                    Text('講義検索'),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop('Save$newLecture');
+                        },
+                        child: Text('保存')),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop('Delete');
+                        },
+                        child: Text('削除')),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text('講義名または教授名で検索'),
+                  ],
+                ),
+                Row(
+                  children: [
+                    IconButton(
+                      onPressed: () async {
+                        var searchResult = await showSearch(
+                          context: context,
+                          delegate: CustomSearchDelegate(
+                            cellText: currentCellNum,
+                          ),
+                        );
+                        if (searchResult != null)
+                          setState(() {
+                            newLecture = searchResult;
+                          });
+                        else
+                          setState(() {
+                            newLecture = 'searchResult is null';
+                          });
+                      },
+                      icon: const Icon(Icons.search),
+                    )
+                  ],
+                ),
+                SafeArea(
+                  child: Row(
+                    children: [
+                      Flexible(
+                          child: Text(newLecture.isEmpty
+                              ? '講義名: $currentLecture'
+                              : '講義名: $newLecture')),
                     ],
                   ),
-                  SafeArea(
-                    child: Row(
-                      children: [
-                        Flexible(
-                            child: Text(searchResult.isEmpty
-                                ? '講義名: $currentLecture'
-                                : '講義名: $searchResult')),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         );
--- lib/screens/test_page.dart
+++ lib/screens/test_page.dart
@@ -1,92 +1,55 @@
 import 'package:flutter/material.dart';
 
-class EditableBox extends StatefulWidget {
-  final String text;
-  final Function(String) onSave;
-
-  EditableBox({required this.text, required this.onSave});
-
-  @override
-  _EditableBoxState createState() => _EditableBoxState();
-}
-
-class _EditableBoxState extends State<EditableBox> {
-  bool isEditing = false;
-  late TextEditingController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = TextEditingController(text: widget.text);
-  }
+class MyCustomLayout extends StatelessWidget {
+  const MyCustomLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(5),
-      padding: EdgeInsets.all(5),
-      height: 100,
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        border: Border.all(color: Colors.grey),
-        borderRadius: BorderRadius.circular(5),
-      ),
-      child: Stack(
-        children: <Widget>[
-          Align(
-            alignment: Alignment.center,
-            child: isEditing
-                ? TextField(
-                    controller: _controller,
-                  )
-                : Text(_controller.text),
+    return Row(
+      children: <Widget>[
+        Expanded(
+          flex: 2,
+          child: Container(
+            child: Table(
+              defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+              children: [
+                _buildTableRow("Header 1", flex: 1),
+                _buildTableRow("Content 1", flex: 2),
+                _buildTableRow("Header 2", flex: 1),
+                _buildTableRow("Content 2", flex: 2),
+                _buildTableRow("Header 3", flex: 1),
+                _buildTableRow("Content 3", flex: 2),
+              ],
+            ),
           ),
-          if (isEditing) ...[
-            Align(
-              alignment: Alignment.bottomLeft,
-              child: IconButton(
-                icon: Icon(Icons.cancel),
-                onPressed: () {
-                  setState(() {
-                    _controller.text = widget.text;
-                    isEditing = false;
-                  });
-                },
-              ),
+        ),
+        Expanded(
+          flex: 8,
+          child: Container(
+            child: Table(
+              defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+              children: [
+                _buildTableRow("Row 1", flex: 1),
+                _buildTableRow("Row 2", flex: 2),
+              ],
             ),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: IconButton(
-                icon: Icon(Icons.check),
-                onPressed: () {
-                  setState(() {
-                    widget.onSave(_controller.text);
-                    isEditing = false;
-                  });
-                },
-              ),
-            ),
-          ] else
-            Align(
-              alignment: Alignment.bottomRight,
-              child: IconButton(
-                icon: Icon(Icons.edit),
-                onPressed: () {
-                  setState(() {
-                    isEditing = true;
-                  });
-                },
-              ),
-            ),
-        ],
-      ),
+          ),
+        ),
+      ],
     );
   }
 
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
+  TableRow _buildTableRow(String text, {required int flex}) {
+    return TableRow(
+      children: [
+        FractionallySizedBox(
+          heightFactor: flex.toDouble() / 3,
+          child: Container(
+            color: Colors.lightBlue,
+            child: Center(child: Text(text)),
+          ),
+        ),
+      ],
+    );
   }
 }
